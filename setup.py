cef20f00b15a55c5a25bae0133ac8566b64dcaae
Olivier Carrere (ocarrere) <ocarrere@cisco.com>
2013-09-10 15:20:13 +0100

    Set version to 0.1.6 in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ TEST_REQUIREMENTS = read_requirements('requirements-test.txt')
 
 setup(
     name='Flask-CuddlyRest',
-    version='0.1.5',
+    version='0.1.6',
     url='http://github.com/wuurrd/flask-cuddlyrest',
     license='MIT',
     author='David Buchmann',
